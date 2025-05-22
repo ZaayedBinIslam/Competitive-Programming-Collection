87e840425e8e8b40fcce2a0cf16ac239ece7e89b
ZaayedBinIslam <zaayed.bin.islam@gmail.com>
2025-05-22 19:10:52 +0600

    added template

---
+++ Code Template/Template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#ifndef ONLINE_JUDGE
+#define REDIRECT_IO \
+    freopen("input.txt", "r", stdin);   \
+    freopen("output.txt", "w", stdout); \
+    freopen("error.txt", "w", stderr);
+#else
+#define REDIRECT_IO
+#endif
+
+
+#define faster ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define getUnique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+#define nl '\n'
+
+
+int main() {
+    faster;
+    REDIRECT_IO;
+    int t;
+    cin >> t;
+    while (t--) {
+
+    }
+    return 0;
+}
