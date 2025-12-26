0f5f270a5c15412847b315477854fe22e342fb19
ZaayedBinIslam <zaayed.bin.islam@gmail.com>
2025-12-26 08:29:36 +0600

    template updated with sieve

--- Template.cpp
+++ Template.cpp
@@ -6,24 +6,32 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
-#ifndef ONLINE_JUDGE
-#define REDIRECT_IO \
-    freopen("input.txt", "r", stdin);   \
-    freopen("output.txt", "w", stdout); \
-    freopen("error.txt", "w", stderr);
-#else
-#define REDIRECT_IO
-#endif
-
-
 #define faster ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define getUnique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
 #define nl '\n'
 
+const int M = 1000000;
+bool      marked[M];
+bool      isprime(int n) { // call sieve then check with this
+    if (n == 2)
+        return true;
+    if (n < 2 || n % 2 == 0)
+        return false;
+    return marked[n] == false;
+}
+void sieve(ll n) {
+    for (int i = 3; i * i <= n; i += 2) {
+        if (marked[i] == false) // i is a prime
+        {
+            for (int j = i * i; j <= n; j += i) {
+                marked[j] = true; // true marked are not prime
+            }
+        }
+    }
+}
 
 int main() {
     faster;
-    REDIRECT_IO;
     int t;
     cin >> t;
     while (t--) {
